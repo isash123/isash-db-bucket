925fa29d7f3c7b59b6195e631b3e1ae5eacc31ee
Shashank Kapoor <shashank.kapoor123@gmail.com>
2024-01-28 15:22:33 +0000

    dumpToFile2

--- src/backup.ts
+++ src/backup.ts
@@ -40,50 +40,38 @@ const dumpToFile = async (filePath: string) => {
   console.log("Dumping DB to file...");
 
   await new Promise((resolve, reject) => {
-    const dumpProcess = exec(`pg_dump -d ${env.BACKUP_DATABASE_URL} -Ft`, (error, stdout, stderr) => {
+    exec(`pg_dump -d ${env.BACKUP_DATABASE_URL} -Ft | gzip > ${filePath}`, (error, stdout, stderr) => {
       if (error) {
         reject({ error: error, stderr: stderr.trimEnd() });
         return;
       }
 
-      // Redirect stdout to the specified file
-      const writeStream = createWriteStream(filePath);
-      writeStream.write(stdout);
-      writeStream.end();
+      // check if archive is valid and contains data
+      const isValidArchive = (execSync(`gzip -cd ${filePath} | head -c1`).length == 1) ? true : false;
+      if (isValidArchive == false) {
+        reject({ error: "Backup archive file is invalid or empty; check for errors above" });
+        return;
+      }
 
-      dumpProcess.on('exit', (code) => {
-        if (code === 0) {
-          // check if archive is valid and contains data
-          const isValidArchive = (execSync(`gzip -cd ${filePath} | head -c1`).length == 1);
-          if (!isValidArchive) {
-            reject({ error: "Backup archive file is invalid or empty; check for errors above" });
-            return;
-          }
+      // not all text in stderr will be a critical error, print the error / warning
+      if (stderr != "") {
+        console.log({ stderr: stderr.trimEnd() });
+      }
 
-          // not all text in stderr will be a critical error, print the error / warning
-          if (stderr !== "") {
-            console.log({ stderr: stderr.trimEnd() });
-          }
+      console.log("Backup archive file is valid");
+      console.log("Backup filesize:", filesize(statSync(filePath).size));
 
-          console.log("Backup archive file is valid");
-          console.log("Backup filesize:", filesize(statSync(filePath).size);
+      // if stderr contains text, let the user know that it was potently just a warning message
+      if (stderr != "") {
+        console.log(`Potential warnings detected; Please ensure the backup file "${path.basename(filePath)}" contains all needed data`);
+      }
 
-          // if stderr contains text, let the user know that it was potentially just a warning message
-          if (stderr !== "") {
-            console.log(`Potential warnings detected; Please ensure the backup file "${path.basename(filePath)}" contains all needed data`);
-          }
-
-          resolve(undefined);
-        } else {
-          reject({ error: "pg_dump process exited with a non-zero code." });
-        }
-      });
+      resolve(undefined);
     });
   });
 
   console.log("DB dumped to file...");
 }
-
 
 const deleteFile = async (path: string) => {
   console.log("Deleting file...");
@@ -101,7 +89,7 @@ export const backup = async () => {
 
   const date = new Date().toISOString();
   const timestamp = date.replace(/[:.]+/g, '-');
-  const filename = `${env.BACKUP_PROJECT_NAME}-${timestamp}.dump`;
+  const filename = `${env.BACKUP_PROJECT_NAME}-${timestamp}.dump.gz`;
   const filepath = path.join(os.tmpdir(), filename);
 
   await dumpToFile(filepath);
