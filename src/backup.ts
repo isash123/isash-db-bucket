0976ae3647952a48bafb2d7e8ee46c1f6f6d09d9
Shashank Kapoor <shashank.kapoor123@gmail.com>
2024-01-28 19:26:34 +0000

    finalfix

--- src/backup.ts
+++ src/backup.ts
@@ -66,22 +66,6 @@ const dumpToFile = async (filePath: string) => {
   console.log("DB dumped to file...");
 }
 
-const zipAndPasswordProtect = async (filePath: string) => {
-  console.log("Zipping and password protecting file...");
-
-  await new Promise((resolve, reject) => {
-    exec(`zip -P ${env.BACKUP_PASSWORD} -r ${filePath}.zip ${filePath}`, (error, stdout, stderr) => {
-      if (error) {
-        reject({ error: error, stderr: stderr.trimEnd() });
-        return;
-      }
-
-      console.log("File zipped and password protected...");
-      resolve(undefined);
-    });
-  });
-}
-
 const deleteFile = async (path: string) => {
   console.log("Deleting file...");
   await new Promise((resolve, reject) => {
@@ -98,14 +82,12 @@ export const backup = async () => {
 
   const date = new Date().toISOString();
   const timestamp = date.replace(/[:.]+/g, '-');
-  const filename = `${env.BACKUP_PROJECT_NAME}-${timestamp}`; // Remove .dump extension
+  const filename = `${env.BACKUP_PROJECT_NAME}-${timestamp}.dump`;
   const filepath = path.join(os.tmpdir(), filename);
 
-  await dumpToFile(filepath + ".dump"); // Add .dump extension here
-  await zipAndPasswordProtect(filepath);
-  await uploadToS3({ name: `${filename}.zip`, path: `${filepath}.zip` });
-  await deleteFile(filepath + ".dump");
-  await deleteFile(`${filepath}.zip`);
+  await dumpToFile(filepath);
+  await uploadToS3({ name: filename, path: filepath });
+  await deleteFile(filepath);
 
   console.log("DB backup complete...");
 }
