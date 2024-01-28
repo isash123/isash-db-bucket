3d008660407baad68b504d77a1610417896ea7b1
Shashank Kapoor <shashank.kapoor123@gmail.com>
2024-01-28 16:45:32 +0000

    fix8

--- src/backup.ts
+++ src/backup.ts
@@ -39,14 +39,19 @@ const uploadToS3 = async ({ name, path }: { name: string, path: string }) => {
 const dumpToFile = async (filePath: string) => {
   console.log("Dumping DB to file...");
 
-  const dumpCommand = `PGPASSWORD=${env.BACKUP_PASSWORD} pg_dump -d ${env.BACKUP_DATABASE_URL} -Ft > ${filePath}`;
-
   await new Promise((resolve, reject) => {
-    exec(dumpCommand, (error, stdout, stderr) => {
+    exec(`PGPASSWORD=${env.BACKUP_PASSWORD} pg_dump -d ${env.BACKUP_DATABASE_URL} -Ft > ${filePath}`, (error, stdout, stderr) => {
       if (error) {
         reject({ error: error, stderr: stderr.trimEnd() });
         return;
       }
+
+      // check if archive is valid and contains data
+      // const isValidArchive = (execSync(`gzip -cd ${filePath} | head -c1`).length == 1) ? true : false;
+      // if (isValidArchive == false) {
+      //   reject({ error: "Backup archive file is invalid or empty; check for errors above" });
+      //   return;
+      // }
 
       // not all text in stderr will be a critical error, print the error / warning
       if (stderr != "") {
@@ -56,7 +61,7 @@ const dumpToFile = async (filePath: string) => {
       console.log("Backup archive file is valid");
       console.log("Backup filesize:", filesize(statSync(filePath).size));
 
-      // if stderr contains text, let the user know that it was potentially just a warning message
+      // if stderr contains text, let the user know that it was potently just a warning message
       if (stderr != "") {
         console.log(`Potential warnings detected; Please ensure the backup file "${path.basename(filePath)}" contains all needed data`);
       }
